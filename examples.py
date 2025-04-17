4415b9406cb80e5b3bd2a660ebc0d8b0382d7852
Bernando Cribbenza <bernandocribbenza@gmail.com>
2025-04-17 12:01:39 +0100

    improve examples

--- examples.py
+++ examples.py
@@ -3,44 +3,49 @@ import ubon_pycstuff.ubon_pycstuff as upyc
 import cv2
 import stuff
 
-def random_test():
+
+def basic_test(jpeg_file):
     bgr_img = cv2.imread("/mldata/image/arrest.jpg")
     rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
 
-    img = upyc.c_image.from_numpy(rgb_img)
+    # copy image to C domain
+    img = upyc.c_image.from_numpy(rgb_img) # will be RGB24_HOST
 
-    upyc.cuda_set_sync_mode(False, False)
-    #img.display("newly created")
-    img_scaled = img.scale(1280, 720)
-    #img_scaled.display("scaled")
-    #img_converted = img.convert(upyc.YUV420_DEVICE)
-    #img_converted.display("converted")
-    #img_converted2 = img_converted.convert(upyc.NV12_DEVICE)
-    #img_converted2.display("converted2")
+    #scale to 720p
+    img_scaled = img.scale(1280, 720) # implicily will get copied to GPU and converted to YUV420
 
-    # Convert back to BGR for OpenCV display
-    print(img.hash(), img_scaled.hash())
+    # copy back and display
     round_trip_bgr = cv2.cvtColor(img_scaled.to_numpy(), cv2.COLOR_RGB2BGR)
 
-    # Display the image
     cv2.imshow("Round-trip image", round_trip_bgr)
-    cv2.waitKey(10)
-    cv2.waitKey(10)
     cv2.waitKey(5000)
     cv2.destroyAllWindows()
 
+def test_inference(jpeg_file):
+    bgr_img = cv2.imread("/mldata/image/arrest.jpg")
+    rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+    img = upyc.c_image.from_numpy(rgb_img) # will be RGB24_HOST
+
     # run tensortRT inference
     inf = upyc.c_infer("/mldata/weights/trt/yolo11l-dpa-131224.trt", "")
-    dets=inf.run(img_scaled)
-    print(dets)
+    dets=inf.run(img)
+    for d in dets:
+        print(d)
 
+    display=stuff.Display(1280,720)
+    for d in dets:
+        display.draw_box(d["box"], thickness=2, clr=(255, 255, 0, 0))
+    display.show(rgb_img, is_rgb=True)
+    display.get_events(5000)
+
+def test_nvof(h264_file):
     # create a NVDEC video decoder
     decoder = upyc.c_decoder()
-    with open("/mldata/video/MOT20-01.264", "rb") as f:
+    with open(h264_file, "rb") as f:
         bitstream = f.read()
 
     # decode some video
-    frames = decoder.decode(bitstream)
+    frames = decoder.decode(bitstream) 
 
     # create an NVOF optical flow engine
     flow_engine = upyc.c_nvof(320, 320)
@@ -52,8 +57,7 @@ def random_test():
             continue
         print(f"frame {i}")
         arr = frame.to_numpy()
-        dets=inf.run(frame)
-        #print(dets)
+
         costs, flow = flow_engine.run(frame) # returns np arrays with costs, flow vectors
         h,w=costs.shape
         d.clear()
@@ -67,19 +71,15 @@ def random_test():
                 start=[(x+0.5)/w, (y+0.5)/h]
                 dx=flow[y][x][0]
                 dy=flow[y][x][1]
-                #print(x,y,costs[x][y],dx,dy)
+
                 stop=[start[0]+flow[y][x][0], start[1]+flow[y][x][1]]
                 if abs(dx)>0.001 or abs(dy)>0.001:
                     d.draw_line(start, stop, clr=(255,255,255,255))
         d.show(arr, is_rgb=True)
         d.get_events(30)
 
-    # Load JPEG image using OpenCV
-    bgr_img = cv2.imread("/mldata/image/arrest.jpg")
-    rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
-
-    img = upyc.c_image.from_numpy(rgb_img)
-
-random_test()
+basic_test("/mldata/image/arrest.jpg")
+test_inference("/mldata/image/arrest.jpg")
+test_nvof("/mldata/video/MOT20-01.264")
 
 
