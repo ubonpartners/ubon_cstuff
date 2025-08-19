1e3328fecfe3cd77fecc0816c2b491db6a3c83ed
Bernando Cribbenza <bernandocribbenza@gmail.com>
2025-08-19 13:41:25 +0100

    mp4_test: cope with special NALU file format

--- src/apps/mp4_test.c
+++ src/apps/mp4_test.c
@@ -20,26 +20,80 @@ int main(int argc, char *argv[])
     image_init();
     const char *infile=argv[1];
 
+    bool john_format=false;
+
     float fps=file_decoder_parse_fps(infile);
     simple_decoder_codec_t codec=file_decoder_parse_codec(infile);
+    if (codec==SIMPLE_DECODER_CODEC_UNKNOWN)
+    {
+        log_warn("Assuming file is in John's NALU format, with H264");
+        codec=SIMPLE_DECODER_CODEC_H264;
+        john_format=true;
+    }
+    else
+    {
+        log_warn("Assuming file is Annex-B format, detected codec %s, %ffps ",
+            (codec==SIMPLE_DECODER_CODEC_H264) ? "H264" : "H265", fps);
+    }
 
-    mp4_writer_t *wr=mp4_writer_create("out1.mp4", (codec==SIMPLE_DECODER_CODEC_H265));
+    const char *out="out.mp4";
+    if (argc>2) out=argv[2];
+
+    mp4_writer_t *wr=mp4_writer_create(out, (codec==SIMPLE_DECODER_CODEC_H265));
     h26x_assembler_t *a=h26x_assembler_create((codec==SIMPLE_DECODER_CODEC_H264) ? H26X_CODEC_H264 : H26X_CODEC_H265,
                                               wr,cb,true);
 
-    printf("opening %s\n",argv[1]);
+    log_info("opening %s",argv[1]);
+
     FILE *f=fopen(argv[1], "rb");
-    while(1)
+
+    if (john_format==false)
     {
-        uint8_t temp[2048];
-        size_t r=fread(temp, 1, 2048, f);
-        h26x_assembler_process_raw_video(a,
-                                     fps,
-                                     temp,
-                                     (int)r);
-        if (r<2048) break;
+        while(1)
+        {
+            uint8_t temp[2048];
+            size_t r=fread(temp, 1, 2048, f);
+            h26x_assembler_process_raw_video(a,
+                                        fps,
+                                        temp,
+                                        (int)r);
+            if (r<2048) break;
+        }
     }
-    printf("done!");
+    else
+    {
+        int nalu_buf_size=1024*1024;
+        uint8_t *nalu_buf=(uint8_t *)malloc(nalu_buf_size);
+        uint64_t first_timestamp=0;
+        bool first_nalu=true;
+        while(1)
+        {
+            uint64_t ts;
+            uint32_t len;
+            if (8!=fread(&ts, 1, 8, f)) break;
+            if (4!=fread(&len, 1, 4, f)) break;
+            len=__builtin_bswap32(len);
+            ts=__builtin_bswap64(ts);
+
+            assert(len<1024*1024);
+            if (len!=(int)fread(nalu_buf+4, 1, len, f)) break;
+
+            if (ts==0) continue; // HACK : ignore initial extra TS=0 packets
+            if (first_nalu)
+            {
+                first_nalu=false;
+                first_timestamp=ts;
+            }
+            double time=(ts-first_timestamp)/90000.0;
+
+            nalu_buf[0]=(len>>24)&0xff;
+            nalu_buf[1]=(len>>16)&0xff;
+            nalu_buf[2]=(len>>8)&0xff;
+            nalu_buf[3]=(len>>0)&0xff;
+            (void)h26x_assembler_process_nalus(a, nalu_buf, len+4, time);
+        }
+    }
+    log_info("all done, written %s", out);
     mp4_writer_destroy(wr);
     return 1;
 }
